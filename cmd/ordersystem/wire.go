a5954e37b2fd3b9cd588a477531217652587479c
tiago-kimura <kimura.tiago@gmail.com>
2024-10-18 10:27:14 -0300

    feat: added wire and grpc layer

---
+++ cmd/ordersystem/wire.go
@@ -0,0 +1,51 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"database/sql"
+
+	"github.com/google/wire"
+	"github.com/tiago-kimura/cleanArchitecture/internal/entity"
+	"github.com/tiago-kimura/cleanArchitecture/internal/infra/database"
+	"github.com/tiago-kimura/cleanArchitecture/internal/infra/event"
+	"github.com/tiago-kimura/cleanArchitecture/internal/infra/web"
+	"github.com/tiago-kimura/cleanArchitecture/internal/usecase"
+	"github.com/tiago-kimura/cleanArchitecture/pkg/events"
+)
+
+var setOrderRepositoryDependency = wire.NewSet(
+	database.NewOrderRepository,
+	wire.Bind(new(entity.OrderRepositoryInterface), new(*database.OrderRepository)),
+)
+
+var setEventDispatcherDependency = wire.NewSet(
+	events.NewEventDispatcher,
+	event.NewOrderCreated,
+	wire.Bind(new(events.EventInterface), new(*event.OrderCreated)),
+	wire.Bind(new(events.EventDispatcherInterface), new(*events.EventDispatcher)),
+)
+
+var setOrderCreatedEvent = wire.NewSet(
+	event.NewOrderCreated,
+	wire.Bind(new(events.EventInterface), new(*event.OrderCreated)),
+)
+
+func NewCreateOrderUseCase(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *usecase.CreateOrderUseCase {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setOrderCreatedEvent,
+		usecase.NewCreateOrderUseCase,
+	)
+	return &usecase.CreateOrderUseCase{}
+}
+
+func NewWebOrderHandler(db *sql.DB, eventDispatcher events.EventDispatcherInterface) *web.WebOrderHandler {
+	wire.Build(
+		setOrderRepositoryDependency,
+		setOrderCreatedEvent,
+		web.NewWebOrderHandler,
+	)
+	return &web.WebOrderHandler{}
+}
